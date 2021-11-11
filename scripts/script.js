89e39adb2d715a8c37d3ef6be417d8d5c4d6b363
Olga Tolchkova <olchiktolchkova@yandex.ru>
2021-11-11 11:07:31 +0400

    Настроить смену пользовательских данных

--- scripts/script.js
+++ scripts/script.js
@@ -3,28 +3,18 @@ const closePopup = document.querySelector('.popup__close-button');
 const openPopup = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 const saveButton = document.querySelector('.popup__save-button');
+const savePopup = document.querySelector('.popup__form');
 let profileName = document.querySelector('.profile__name');
 let profileInterest = document.querySelector('.profile__interest');
-let nameInput = document.querySelector('.popup__input_name');
-let interestInput = document.querySelector('.popup__input_interest');
-
-
-// Функция обработчик сработает, в момент отправки 
-// из формы в профиль
-
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    nameInput.value = profileName.textContent;
-    interestInput.value = profileInterest.textContent;
-    closeClick();
-}
+let nameInput = document.querySelector('.popup__input_type_name');
+let interestInput = document.querySelector('.popup__input_type_interest');
 
 
 // открытие попапа
 function openClick() {
     popupElement.classList.add('popup_opened');
-
-    formSubmitHandler(evt);
+    nameInput.value = profileName.textContent;
+    interestInput.value = profileInterest.textContent;
 }
 
 // закрытие попапа
@@ -33,7 +23,7 @@ function closeClick() {
 }
 
 // сохранение
-function saveClick() {
+function saveClick(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileInterest.textContent = interestInput.value;
@@ -41,6 +31,5 @@ function saveClick() {
 }
 
 openPopup.addEventListener('click', openClick);
-popupElement.addEventListener('submit', formSubmitHandler);
 closePopup.addEventListener('click', closeClick);
-saveButton.addEventListener('click', saveClick);
+savePopup.addEventListener('submit', saveClick);
